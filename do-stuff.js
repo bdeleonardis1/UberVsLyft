a7998aac074d038654ca9597ba8431677679c4ce
Brian DeLeonardis <bdeleonardis1@gmail.com>
2016-09-10 16:52:41 -0700

    Now has GPS coordinates for both addresses.

--- do-stuff.js
+++ do-stuff.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
 	var uberPrice = 10.11;
-	var lyftPrice = 20.54;
+	var lyftPrice = 8.54;
 	var uberTime = 10.00;
 	var lyftTime = 4.50;
+	
+	var lat = 0;
+	var lon = 0;
 	
 	function getUp()
 	{
@@ -51,20 +54,46 @@ $(document).ready(function(){
 		return mess;
 	}
 	
+	function getCoord(address, fn){
+		var geocoder = new google.maps.Geocoder();
+		geocoder.geocode( { 'address': address}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			fn(results[0].geometry.location);
+		  } 
+		});
+	}
+	
+	var x = 2
 	$('#compare').click(function(){
-		var up = getUp();
-		var lp = getLp();
-		var ut = getUt();
-		var lt = getLt();
+		address1 = $('#a').val();
+		address2 = $('#b').val();
 		
-		$('#table').css('visibility', 'visible');
-		$('#uber-price').html('$' + getUp())
-		$('#lyft-price').html('$' + getLp())
-		$('#uber-time').html(getUt())
-		$('#lyft-time').html(getLt())
+		getCoord(address1, function(coord){
+			var lat1 = coord.lat();
+			var lon1= coord.lng();
+			
+			getCoord(address2, function(coord){
+				var lat2 = coord.lat();
+				var lon2 = coord.lng();
+						
+				var up = getUp();
+				var lp = getLp();
+				var ut = getUt();
+				var lt = getLt();
+				
+				$('#table').css('visibility', 'visible');
+				$('#uber-price').html('$' + getUp())
+				$('#lyft-price').html('$' + getLp())
+				$('#uber-time').html(getUt())
+				$('#lyft-time').html(getLt())
+				
+				$('#message').text(getMessage(up, lp, ut, lt)) 
+			});
+		});
 		
-		$('#message').text(getMessage(up, lp, ut, lt))
 	});
+	
+	
 	
 
 	
